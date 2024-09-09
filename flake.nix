8db1c3dbda5fa5bf09ea5a4a9ee26bd5b543b548
Alexandru Nechita <alexandru.italia32@gmail.com>
2024-09-09 22:46:39 +0200

    build(nix): Migrate to nix flake build

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zmk-nix }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+  in {
+    packages = forAllSystems (system: rec {
+      default = firmware;
+
+      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+        name = "firmware";
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [ ".conf" ".keymap" ".dtsi" ".yml" ".shield" ".overlay" ".defconfig" ];
+
+        board = "nice_nano_v2";
+        shield = "urchin_%PART% nice_view_adapter nice_view";
+
+        zephyrDepsHash = "sha256-MFIMeQe8fat2IbhkMwnj8cMbm0kUO15kOdN6HTPA8rk=";
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
+}
